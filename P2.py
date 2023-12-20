885872a3d4f5776e2b0fc74af46af94c2d4ad688
Prajwal Kamath <125379835+prajwalkamath03@users.noreply.github.com>
2023-12-20 10:03:49 +0530

    Create P2.py

---
+++ P2.py
@@ -0,0 +1,37 @@
+import numpy as np
+#create arrays
+a=np.array([1,2,3,4,5])
+b=np.array([6,7,8,9,10])
+
+#basic operations
+print("Array a:",a)
+print("Array b:",b)
+print("Sum of arrays a and b:",np.add(a,b))
+print("Difference of arrays a and b:",np.subtract(a,b))
+print("Product of arrays a and b:",np.multiply(a,b))
+print("Division of arrays a and b:",np.divide(a,b))
+print("Square root of arrays a:",np.sqrt(a))
+print("Exponential of arrays a:",np.exp(a))
+
+
+#Aggregation operations
+print("Minimum value of array a:",np.min(a))
+print("Minimum value of array b:",np.min(b))
+print("Mean of array a:",np.mean(a))
+print("Standard deviation of array a:",np.std(a))
+
+
+
+# OUTPUT:
+# Array a: [1 2 3 4 5]
+# Array b: [ 6  7  8  9 10]
+# Sum of arrays a and b: [ 7  9 11 13 15]
+# Difference of arrays a and b: [-5 -5 -5 -5 -5]
+# Product of arrays a and b: [ 6 14 24 36 50]
+# Division of arrays a and b: [0.16666667 0.28571429 0.375      0.44444444 0.5       ]
+# Square root of arrays a: [1.         1.41421356 1.73205081 2.         2.23606798]
+# Exponential of arrays a: [  2.71828183   7.3890561   20.08553692  54.59815003 148.4131591 ]
+# Minimum value of array a: 1
+# Minimum value of array b: 6
+# Mean of array a: 3.0
+# Standard deviation of array a: 1.4142135623730951
