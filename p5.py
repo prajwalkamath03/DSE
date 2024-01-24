10416ab1fa71651d6855f0ce6c278bd955a6865d
Prajwal Kamath <125379835+prajwalkamath03@users.noreply.github.com>
2024-01-24 09:54:10 +0530

    Update p5.py

--- p5.py
+++ p5.py
@@ -2,8 +2,9 @@ import matplotlib.pyplot as plt
 import random
 import math
 from collections import Counter
-def inverse_normal_cdf(p,mu=0,sigma=1,tolerane=0.00001):
-    if mu!=0 or sigma!=1:
+
+def inverse_normal_cdf(p, mu=0, sigma=1, tolerance=0.00001):
+    if mu != 0 or sigma != 1:
         return mu + sigma * inverse_normal_cdf(p, tolerance=tolerance)
     low_z, low_p = -10.0, 0
     hi_z, hi_p = 10.0, 1
@@ -17,21 +18,21 @@ def inverse_normal_cdf(p,mu=0,sigma=1,tolerane=0.00001):
         else:
             break
     return mid_z
+
 def normal_cdf(x, mu=0, sigma=1):
     return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
+
 def random_normal():
     return inverse_normal_cdf(random.random())
-xs=[random_normal()for _ in range(1000)]
-ys1=[x+random_normal()/2 for x in xs]
-ys2=[-x+random_normal()/2 for x in xs]
 
-plt.scatter(xs.ys1,marker='.',color='black',label='ys1')
-plt.scatter(xs.ys2,marker='.',color='gray',label='ys2')
+xs = [random_normal() for _ in range(1000)]
+ys1 = [x + random_normal() / 2 for x in xs]
+ys2 = [-x + random_normal() / 2 for x in xs]
+
+plt.scatter(xs, ys1, marker='.', color='black', label='ys1')
+plt.scatter(xs, ys2, marker='.', color='gray', label='ys2')
 plt.xlabel('xs')
 plt.ylabel('ys')
-plt.legend(loc=9)
+plt.legend(loc=9)  # Change the legend location if needed
 plt.title("Very Different Joint Distributions")
 plt.show()
-
-
-        
