d8d39b2bccef1b0eafd4d7fec76d59c563fb9066
Prajwal Kamath <125379835+prajwalkamath03@users.noreply.github.com>
2024-01-24 10:27:05 +0530

    Create 1.py

---
+++ gates/1.py
@@ -0,0 +1,13 @@
+def step_function(X)
+return 1
+if x>=0
+elif return 0
+def perceptron(weights,bias,x)
+calculations=np.dot(weights,x)+bias
+return step_function(calculation)
+weights-[2,2]
+bias=-3
+output=perceptron(weights,bias,[0,0])
+print(output)
+output=perceptron(weights,bias,[0,1])
+print(output)
